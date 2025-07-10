fb3b34f4a03a50d76ef67bea0bbee61be2e5e6b0
Amarsimos <amarsimoss@gmail.com>
2025-07-10 09:38:58 +0800

    add data visable ex

--- project2_datachart/population/world_population.py
+++ project2_datachart/population/world_population.py
@@ -1,16 +1,40 @@
 import json
+from country_code import get_country_code
+import pygal_maps_world.maps
 
 filename = 'Population.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-for pop_dict_country in pop_data.items():
-    for name,data in  pop_dict_country.items():
-        for year in data.items():
+cc_populations = {}
+for pop_dict_country ,pop_dict_value in pop_data.items():
+    try :
+        for key,value in  pop_dict_value.items():
+            if key == 'name':
+                code = get_country_code(value)
+            if key == 'data':
+                for year,data in value.items():
+                    if year == '2100':
+                        population = data
+                        country_name = pop_dict_value['name']
+                        print(country_name + ':' + str(population))
+
+                        if code:
+                            cc_populations[code] = population
+                            # print(code +":" + str(population))
+                        else:
+                            print('ERROR -' + country_name)
+    except:
+        print(pop_dict_country + ':' + str(pop_dict_value))
+
+wm = pygal_maps_world.maps.World()
+wm.title = "World Population in 2100, by Country"
+wm.add('2100',cc_populations)
+
+wm.render_to_file('world_population.svg')
+        # for year,data in value.items():
+        # for year in data.items():
     # for pop_dict_key in {pop_dict_country}:
-            print(year)
 #         if pop_dict_key == '2015':
-#             country_name =pop_dict['Country Name']
-#             population = pop_dict['Value']
 #         print(country_name + ":" + population)
     # print(pop_data)
